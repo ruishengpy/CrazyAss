a1f9fa37fbb17f70c1169bac0246d6e0c9041f2b
李瑞升 <liruisheng@edaijia-inc.cn>
2018-01-08 11:47:38 +0700

    modiresd

--- CrazyAss/settings.py
+++ CrazyAss/settings.py
@@ -124,5 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STITIC
 SESSION_TRACKER_SCRIPT='%s/backend/session_tracker.sh' % BASE_DIR
 AUDIT_LOG_PATH='%s/log/audit' %BASE_DIR
